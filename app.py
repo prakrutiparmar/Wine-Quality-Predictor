6f636fe26e4a79652b058d47d24290a6e05cf641
prakruti parmar <prakruti.parmar96@gmail.com>
2025-08-03 01:23:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle 
+import numpy as np
+
+with open('wine_quality_pred.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+st.header("Wine Quality Prediction")
+st.subheader("Adjust the sliders to predict the quality of a wine quality")
+
+votaltile_acidity = st.slider("Volatile Acidity",0.0,2.0)
+citric_acid = st.slider("Citric Acid",0.0,1.0)
+sulphates = st.slider("Sulphates",0.0,2.0)
+alcohol = st.slider("Alcohol",0.0,15.0)
+
+input_data = [votaltile_acidity,citric_acid,sulphates,alcohol]
+
+if st.button("Predict"):
+    result = model.predict([input_data])
+    if result[0] == 1:
+        st.success("Wine quality is Good")
+    else:
+        st.error("Wine quality is Bad")
+
